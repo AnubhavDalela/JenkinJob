99aa2276dc86de5d2680336763f6fa0e024b1068
SG0305808 <anubhav.dalela.ctr@sabre.com>
2020-02-10 22:39:02 +0530

    changes realted to files

---
+++ src/test/java/TestClasses/GloOpen.java
@@ -0,0 +1,25 @@
+package TestClasses;
+
+import PageObjects.GloPageLogin;
+import Utility.BaseClass;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GloOpen extends BaseClass {
+
+    @Test
+    public void GloLoginPage() throws IOException {
+        driver = initializer();
+
+        driver.get(prop.getProperty("url"));
+
+        GloPageLogin glp = PageFactory.initElements(driver,GloPageLogin.class);
+        glp.Glo_Login("anubhav.dalela","Fwreck1235$");
+        System.out.println("home page");
+
+
+
+    }
+}
